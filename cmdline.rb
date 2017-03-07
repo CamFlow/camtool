62ccd96411b65c9efa30b323cf69da8a36063edb
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-03-07 15:55:22 -0500

    add example file

--- cmdline.rb
+++ cmdline.rb
@@ -5,5 +5,5 @@ require 'rgl/dot'
 require 'rgl/topsort'
 require 'rgl/transitivity'
 
-print CamFlow::ProvJSONtoRGL.new.read_json_file('./json/more.json').information
-CamFlow::MQTTBreaker.new.break_log_file('/tmp/audit.log')
+print CamFlow::ProvJSONtoRGL.new.read_log_file('./log/badlog.log').information
+CamFlow::MQTTBreaker.new.break_log_file('./log/badlog.log')
