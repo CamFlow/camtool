bbb7429c6121fae708fa41c51ef1e4a9765c0ac7
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-06-16 13:20:32 +0100

    add vertex counter

--- cmdline.rb
+++ cmdline.rb
@@ -8,3 +8,4 @@ require 'rgl/transitivity'
 print CamFlow::ProvJSONtoRGL.new.read_log_file('log/audit.log').information
 #CamFlow::MQTTBreaker.new.break_log_file('../audit.log')
 CamFlow::EdgeCounter.new.read_log_file('log/audit.log').show
+CamFlow::VertexCounter.new.read_log_file('log/audit.log').show
