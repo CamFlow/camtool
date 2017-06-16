145488089d9a60ab0ede62d1aabc860b191eb533
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-06-16 13:17:02 +0100

    count number of edge of each type

--- cmdline.rb
+++ cmdline.rb
@@ -5,5 +5,6 @@ require 'rgl/dot'
 require 'rgl/topsort'
 require 'rgl/transitivity'
 
-print CamFlow::ProvJSONtoRGL.new.read_log_file('../audit.log').png
+print CamFlow::ProvJSONtoRGL.new.read_log_file('log/audit.log').information
 #CamFlow::MQTTBreaker.new.break_log_file('../audit.log')
+CamFlow::EdgeCounter.new.read_log_file('log/audit.log').show
--- lib/camflow.rb
+++ lib/camflow.rb
@@ -1,4 +1,5 @@
 require_relative 'provjson/provjson_parser'
 require_relative 'provjson/provjson_to_rgl'
+require_relative 'provjson/edge_counter'
 require_relative 'tools/breaker'
 require_relative 'tools/breaker_mqtt'
