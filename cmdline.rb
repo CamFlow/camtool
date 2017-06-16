f111290d67bc993b9135bb553e4e5365bdec6916
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-06-16 13:50:30 +0100

    add another example

--- cmdline.rb
+++ cmdline.rb
@@ -5,7 +5,7 @@ require 'rgl/dot'
 require 'rgl/topsort'
 require 'rgl/transitivity'
 
-print CamFlow::ProvJSONtoRGL.new.read_log_file('log/audit.log').information
+print CamFlow::ProvJSONtoRGL.new.read_log_file('log/audit2.log').information
 #CamFlow::MQTTBreaker.new.break_log_file('../audit.log')
-CamFlow::EdgeCounter.new.read_log_file('log/audit.log').show
-CamFlow::VertexCounter.new.read_log_file('log/audit.log').show
+CamFlow::EdgeCounter.new.read_log_file('log/audit2.log').show
+CamFlow::VertexCounter.new.read_log_file('log/audit2.log').show
