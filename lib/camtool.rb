b48f5cd094dc51ae2e2536b8dbd282e33edfde82
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-08-07 14:56:31 +0100

    save changes

--- lib/camtool.rb
+++ lib/camtool.rb
@@ -1,6 +1,7 @@
 require 'camtool/provjson_parser'
 require 'camtool/provjson_to_rgl'
 require 'camtool/edge_counter'
+require 'camtool/edge_validator'
 require 'camtool/vertex_counter'
 require 'camtool/log_breaker'
 require 'camtool/breaker_mqtt'
