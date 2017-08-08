2881aff24e66013d6eb032adc2f90db0529bb4a7
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-08-08 16:00:28 +0100

    test all types of edge

--- lib/camtool/edge_validator.rb
+++ lib/camtool/edge_validator.rb
@@ -28,24 +28,28 @@ module CamTool
     end
 
     def used k, v
-      abort "prov:entity is of wrong type:  #{v}." unless is_entity? v['prov:entity']
-      abort "prov:activity is of wrong type: #{v}." unless is_activity? v['prov:activity']
+      abort "used prov:entity is of wrong type:  #{v}." unless is_entity? v['prov:entity']
+      abort "used prov:activity is of wrong type: #{v}." unless is_activity? v['prov:activity']
     end
 
     def wasGeneratedBy k, v
-
+      abort "wasGeneratedBy prov:entity is of wrong type:  #{v}." unless is_entity? v['prov:entity']
+      abort "wasGeneratedBy prov:activity is of wrong type: #{v}." unless is_activity? v['prov:activity']
     end
 
     def wasDerivedFrom k, v
-
+      abort "wasDerivedFrom prov:usedEntity is of wrong type:  #{v}." unless is_entity? v['prov:usedEntity']
+      abort "wasDerivedFrom prov:generatedEntity is of wrong type: #{v}." unless is_entity? v['prov:generatedEntity']
     end
 
     def wasInformedBy k, v
-
+      abort "wasInformedBy prov:informant is of wrong type:  #{v}." unless is_activity? v['prov:informant']
+      abort "wasInformedBy prov:informed is of wrong type: #{v}." unless is_activity? v['prov:informed']
     end
 
     def wasAssociatedWith k, v
-
+      abort "wasAssociatedWith prov:activity is of wrong type:  #{v}." unless is_activity? v['prov:activity']
+      abort "wasAssociatedWith prov:agent is of wrong type: #{v}." unless is_agent? v['prov:agent']
     end
   end
 end
