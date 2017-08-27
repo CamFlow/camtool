d956778298340cb32ecfea584c5e3eaa33b88b83
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-08-27 14:29:53 +0100

    update to support id namespacing

--- lib/camtool/edge_validator.rb
+++ lib/camtool/edge_validator.rb
@@ -4,24 +4,28 @@ module CamTool
     end
 
     def is_relation? id
+      id = id.gsub 'cf:', ''
       bytes = Base64.decode64(id).bytes.to_a
       type = bytes[7] & 0xF0
       return type == 0x80
     end
 
     def is_activity? id
+      id = id.gsub 'cf:', ''
       bytes = Base64.decode64(id).bytes.to_a
       type = bytes[7] & 0xF0
       return type == 0x40
     end
 
     def is_entity? id
+      id = id.gsub 'cf:', ''
       bytes = Base64.decode64(id).bytes.to_a
       type = bytes[7] & 0xF0
       return type == 0x20
     end
 
     def is_agent? id
+      id = id.gsub 'cf:', ''
       bytes = Base64.decode64(id).bytes.to_a
       type = bytes[7] & 0xF0
       return type == 0x10
